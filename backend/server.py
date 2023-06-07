e1f990bf2e39a661fff991b308c95a9daa45d0ef
riru12 <brilljulianrina@gmail.com>
2023-06-07 23:35:02 +0800

    flask back-end file

---
+++ backend/server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from json import *
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def predict():
+    x = 'test'
+    return {
+        'value1':x
+    }
+ 
+     
+# Running app
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
