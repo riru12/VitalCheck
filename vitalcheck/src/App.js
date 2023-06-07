f824529efb4ab07721126ab4a8662a441693b92c
riru12 <brilljulianrina@gmail.com>
2023-06-07 23:33:21 +0800

    flask API back-end  and fetching from react front-end

--- vitalcheck/src/App.js
+++ vitalcheck/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+// Importing modules
+import React, { useEffect } from "react";
+import "./App.css";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (
+        await fetch(
+          "http://localhost:8000"
+        )
+      ).json();
 
+      console.log(data)
+    };
+
+    dataFetch();
+    }, []);
+ 
+    return (
+      <div></div>
+    );
+}
+ 
 export default App;
